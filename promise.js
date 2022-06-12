44ce3dac8bb9163bb654d28c7ad3dca90cea90f0
aliefwijaya <alieffitriyanto92@gmail.com>
2022-06-12 22:19:06 +0700

    challenge-07-alieffw-done

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
+
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {    
+    const hasilixx = await promiseTheaterIXX();
+    const hasilVGC = await promiseTheaterVGC();
+
+    return await Promise.all([hasilixx, hasilVGC])
+    .then((hasil) => {
+      const [resultOfiix,resultofVGC] = hasil;     
+      return[...resultOfiix, ...resultofVGC].filter((jumlahRespon) => jumlahRespon.hasil === emosi).length;
+    })
+  } catch (error) {
+    console.log(`There is an error: ${error}`);
+  }
+}
 
 module.exports = {
   promiseOutput,
